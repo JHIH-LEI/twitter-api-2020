009b6cf63556faad4a926e7ce17ab4f8ad543ef9
JHIH-LEI <vicky88888@gmail.com>
2022-10-22 01:58:02 +0800

    pull followship services into single server by docker

--- routes/followship.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const followshipController = require('../controllers/followshipController')
-
-router.post('/', followshipController.followUser)
-router.delete('/:followingId', followshipController.unFollowUser)
-
-module.exports = router
